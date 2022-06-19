913b5b4ef8edc3fb8530d783cbc0aaadecf1b7f0
RachelLindquist <firefly7491@gmail.com>
2022-06-19 10:26:34 -0400

    Lab 6 pt. 1 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        //A program that converts an inputted temperature in C and provides the equivalent temperature in F.
+        // Hint: Google is your friend! Given C, solve for F.
+        // Again, check for a valid input value and only respond with the F value if you got it,
+        // otherwise output an appropriate error msg to the user.
+        // Testing: 3 conditions: Bad Input, then test for the known freezing and boiling points.
+
+        Scanner in = new Scanner(System.in);
+
+        double cel = 0;
+        String trash = ""; // use for bad input which will read as a String
+        System.out.println("Please enter a temperature in C");
+
+        if(in.hasNextDouble())
+        {
+            cel = in.nextDouble();
+            in.nextLine();
+            double fer = (cel * 9/5) + 32;
+            System.out.println (cel + " converted to Fahrenheit is " + fer + ".");
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println("\nYou entered " + trash + " as a temperature");
+            System.out.println("Run the program again and enter a valid amount!");
+            System.exit(0);  // terminate the program
+        }
     }
 }
